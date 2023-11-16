f3c49132be951aa51c7b9199437ece5d051bb681
lizaaaaaaaaaa <99049229+lizaaaaaaaaaa@users.noreply.github.com>
2023-11-17 02:13:54 +0300

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api
+{
+    public class ValuesController : Controller
+    {
+
+        [HttpGet]
+        [Route("/back")]
+        public string Back()
+        {
+            DateTime time = DateTime.Now;
+            var res = time.ToString();
+            return res;
+        }
+
+    }
+}
